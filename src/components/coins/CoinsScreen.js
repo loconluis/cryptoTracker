adddd34c16aedd25a945220e329bd6c57ed45f5a
Luis Locon <llocon@cognits.co>
2020-10-30 19:06:17 -0600

    first commit

---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const CoinsScreen = () => {
+  return (
+    <View>
+      <Text>Hello Coins</Text>
+    </View>
+  );
+};
+
+export default CoinsScreen;
